09a478980ac0c63c13a4ac423ec1b50d956ff3a4
DangChinh <35798337+DangChinh@users.noreply.github.com>
2019-11-09 21:08:40 -0600

    Add post method and link to back end

--- src/Components/FormRegistration.js
+++ src/Components/FormRegistration.js
@@ -1,25 +1,59 @@
-import React from "react"
+import React, {useState,useEffect} from "react"
 import {Button,Form} from "react-bootstrap"
+import axios from "axios"
 
 
 function FormRegistration() {
+
+    const [username, setUsername] = useState({usernameValue:""})
+    const [userpassword, setUserpassword] = useState({passwordValue:""})
+    const [useremail, setUseremail] = useState({emailValue:""})
+    const [userphone, setUserphone] = useState({phoneValue:""})
+
+    function userChangeHandler(e) {
+        setUsername({usernameValue:e.target.value})
+    }
+    function passwordChangeHandler(e) {
+        setUserpassword({passwordValue:e.target.value})
+    }
+    function emailChangeHandler(e) {
+        setUseremail({emailValue:e.target.value})
+    }
+
+    function phoneChangeHandler(e) {
+        setUserphone({phoneValue:e.target.value})
+    }
+
+    let data = {
+        username: "wiueiweiubw",
+        userpassword: userpassword.passwordValue,
+        useremail: useremail.emailValue,
+        userphone: userphone.phoneValue
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        axios.post("/register", data)
+            .then(response => console.log(response))
+    }
+    
     return (
-           <Form className="registration-form">
+           <Form className="registration-form" onSubmit= {handleSubmit}>
                 <h2>Sign Up</h2>
                 <Form.Group controlId="username">
-                    <Form.Control type="email" placeholder="Your username" />
+                    <Form.Control type="text" placeholder="Your username" value={username.usernameValuealue} onChange={userChangeHandler}/>
                 </Form.Group>
 
                 <Form.Group controlId="userpassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={userpassword.passwordValue} onChange={passwordChangeHandler}/>
                 </Form.Group>
 
                 <Form.Group controlId="useremail">
-                    <Form.Control type="email" placeholder="Enter email..." />
+                    <Form.Control type="email" placeholder="Enter email..." value={useremail.emailValue} onChange={emailChangeHandler} />
                 </Form.Group>
 
                 <Form.Group controlId="userphone">
-                    <Form.Control type="email" placeholder="Your phone number" />
+                    <Form.Control type="tel" placeholder="Your phone number" value={userphone.phoneValue} onChange={phoneChangeHandler}/>
                 </Form.Group>
                 
                 <Button variant="secondary" type="submit">
