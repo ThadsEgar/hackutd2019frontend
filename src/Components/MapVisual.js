659c29edbb4a61bef17105d6616ea987e5ab1316
Thads Egar <tmichaelegar@gmail.com>
2019-11-10 06:51:17 -0600

    map view

--- src/Components/MapVisual.js
+++ src/Components/MapVisual.js
@@ -1,9 +1,10 @@
 import React from "react"
-
+import WebMapView from "./Map.js"
 function MapVisual() {
     return (
         <div className="map">
             <h1>Theres a map</h1>
+            <WebMapView/>
         </div>
     )
 }
